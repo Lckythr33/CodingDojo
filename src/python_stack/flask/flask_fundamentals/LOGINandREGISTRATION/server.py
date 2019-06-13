3a75a68df5b36b765bc942b0300a5cdb7489f580
Z's devBeast <Zeeshawn1@gmail.com>
2019-06-12 18:12:31 -0700

    Finished Login and Registration with Nice Alert Messages and switching logout button

--- src/python_stack/flask/flask_fundamentals/LOGINandREGISTRATION/server.py
+++ src/python_stack/flask/flask_fundamentals/LOGINandREGISTRATION/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, redirect, request, flash, session
 from mysqlconnection import connectToMySQL   # import the function that will return an instance of a connection
 from flask_bcrypt import Bcrypt 
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.secret_key="so secret"
@@ -14,16 +16,22 @@ def create_user():
     is_valid = True
     if len(request.form['first_name']) < 1:
     	is_valid = False
-    	flash("Please enter a first name")
+    	flash("Please enter a first name","first_name")
     if len(request.form['last_name']) < 1:
     	is_valid = False
-    	flash("Please enter a last name")
-    if len(request.form['email']) < 4:
-    	is_valid = False
-    	flash("Email should be at least 2 characters")
+    	flash("Please enter a last name", "last_name")
+    if not EMAIL_REGEX.match(request.form['email']):    # test whether a field matches the pattern
+        flash("Invalid email address!","email")
+        is_valid = False
     if (request.form['password'] != request.form['confirm']):
     	is_valid = False
-    	flash("Passwords do not match!")
+    	flash("Passwords do not match!","password")
+    if not any(x.isupper() for x in request.form['password']):
+    	is_valid = False
+    	flash("Passwords needs uppercase!","upper")
+    if not any(x.isdigit() for x in request.form['password']):
+    	is_valid = False
+    	flash("Passwords needs 1 number!","digit")
 
     if not is_valid:
         return redirect("/")
@@ -38,9 +46,15 @@ def create_user():
         }
         query = "INSERT INTO `logindb`.`users` (`first_name`, `last_name`, `email`, `password`) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);"
         user_id = mysql.query_db(query, data)
+
+        mysql = connectToMySQL('loginDB')	
+        query = f"SELECT * FROM logindb.users WHERE id = {user_id};"
+        result = mysql.query_db(query, data)
+        if not "user" in session:
+            session["user"] = result[0]
     
         print(request.form)
-        flash("User successfully added!")
+        flash("User successfully added!","registered")
         return redirect('/') # either way the application should return to the index and display the message
 
 
@@ -58,12 +72,22 @@ def login():
     if user:
         user = user[0]
         if bcrypt.check_password_hash(user['password'], request.form['password']):
-            flash("User successfully logged in!")
+            flash("User successfully logged in!","loggedin")
+            if not "user" in session:
+                session["user"] = user
+            return redirect('/')
+    
         else:
-            flash("Correct email, wrong password!")
+            flash("Correct email, wrong password!","rightemail")
     else:
-        flash("Invalid Credentials!")
+        flash("Invalid Credentials!","invalidcred")
     return redirect("/")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    flash("User successfully logout","loggedin")
+    return redirect('/')
 
 
 if __name__ == "__main__":
