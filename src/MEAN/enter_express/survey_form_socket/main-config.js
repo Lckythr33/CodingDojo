ba4963edf9ec7fd6c87a6885a306a6aa10bb9291
Z's devBeast <Zeeshawn1@gmail.com>
2019-07-10 23:12:57 -0700

    Survey with Sockets finalized!

--- src/MEAN/enter_express/survey_form_socket/main-config.js
+++ src/MEAN/enter_express/survey_form_socket/main-config.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/static"))
 app.use(session({
-secret: "kitty",
+secret: "venom",
 resave: false,
 saveUninitialized: true,
 cookie: {maxAge: 60000}
